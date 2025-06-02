74438d499979096c520ab03cda396d4ea136e001
Your NameSivngech <you@example.comsivngech2@gmail.com>
2025-06-02 15:15:26 +0700

    changes

--- pages/our-story.vue
+++ pages/our-story.vue
@@ -1,133 +1,122 @@
-<template>
-    <section class="w-full mx-auto px-4 py-10 md:flex items-center justify-center relative">
+<template class="relative">
+    <section class="w-full mx-auto px-4 py-10 md:flex items-center justify-center">
         <div class="flex flex-col md:flex-row overflow-hidden  items-center justify-center">
             <div class="w-full h-[300px] md:h-[450px]">
-                <img class="h-full w-full object-cover" src="public/images/f1.png"
-                    alt="">
+                <img class="h-full w-full object-cover" src="public/images/f1.png" alt="">
             </div>
-            <div class="bg-white rounded shadow p-6 max-w-xl w-full">
 
-                <h1 class="text-3xl md:text-[48px] text-center mb-4 overflow-hidden">Our Story begins</h1>
-                <p class="text-[18px] mb-4">At Poke Now, we believe fast food should be fresh food. From our
-                    premium ingredients to our exceptional service, eating healthy has never been easier—or more
-                    enjoyable! </p>
-                <h1 class="text-[24px] mt-4">What's poke</h1>
-                <p class="text-[18px] mt-2">Poke is a traditional Hawaiian dish that typically consists of
-                    diced raw fish (such as tuna or
-                    salmon)
-                    marinated in soy sauce and other flavorful ingredients. It is often served over a bed of rice and
-                    topped
-                    with various toppings like seaweed, cucumber, avocado, and sesame seeds.</p>
-                <button class="rounded-full text-white w-full sm:w-[219px] h-[52px] mt-6" style="background:#F96540;">Order
-                    Now</button>
+        </div>
+    </section>
+    <div class="bg-white rounded shadow p-6 max-w-xl w-full absolute h-auto md:top-80 md:left-100 z-50">
+        <h1 class="text-3xl md:text-lg text-center overflow-hidden">Our Story begins</h1>
+        <p class="text-sm mb-4">At Poke Now, we believe fast food should be fresh food. From our
+            premium ingredients to our exceptional service, eating healthy has never been easier—or more
+            enjoyable! </p>
+        <h1 class="text-base mt-4">What's poke</h1>
+        <p class="text-sm mt-2">Poke is a traditional Hawaiian dish that typically consists of
+            diced raw fish (such as tuna or
+            salmon)
+            marinated in soy sauce and other flavorful ingredients. It is often served over a bed of rice and
+            topped
+            with various toppings like seaweed, cucumber, avocado, and sesame seeds.</p>
+        <button class="rounded-full text-white w-full md:w-[219px] h-[52px] mt-6" style="background:#F96540;">Order
+            Now</button>
+    </div>
+
+    <section class="max-w-[1440px] py-20 h-full bg-white mx-auto relative">
+        <div class="max-w-6xl mx-auto md:flex  lg:flex-row gap-10 relative z-10">
+
+
+            <div class="flex-1 p-5 text-center lg:text-left overflow-hidden">
+                <h1
+                    class="text-3xl md:text-lg lg:text-[48px] font-semibold mb-4 leading-tight max-w-xl overflow-hidden">
+                    Traditional Heritage, <br class="hidden md:block" />Modern Result
+                </h1>
+                <p class="text-base md:text-lg text-gray-700 max-w-xl mx-auto lg:mx-0 font-semibold">
+                    One of the reasons for poke's popularity is its versatility. While the traditional version
+                    remains a favorite,
+                    there are now numerous variations available, allowing people to customize their poke bowls
+                    according to their
+                    preferences.
+                </p>
+            </div>
+            <div>
+                <img src="public/images/Oval 1.png"
+                    class="relative w-full max-w-[300px] sm:max-w-[340px] md:max-w-[380px] lg:max-w-[400px] z-0 left-0"
+                    alt="Oval background" />
+            </div>
+            <img src="public/images/f2.png"
+                class="absolute top-1/2 left-200 w-[200px] sm:w-[230px] md:w-[260px] transform -translate-x-1/2 -translate-y-1/2 z-10"
+                alt="Main Bowl" />
+
+        </div>
+        <img src="public/images/f3.png" class="absolute top-0 left-300 w-[80px] sm:w-[90px] md:w-[100px] z-20"
+            alt="Leaf Decor" />
+        <img src="public/images/f4.png"
+            class="absolute bottom-[-30px] left-300 w-[70px] sm:w-[80px] md:w-[90px] z-20" alt="Leaf Decor 2" />
+
+    </section>
+
+
+
+    <section class="h-full mx-auto px-4 md:px-10 py-10 max-w-[1440px] md:flex items-center justify-center">
+        <div class="w-full flex flex-col md:flex-row bg-black text-white px-6 py-8 rounded-lg gap-6">
+            <div class="flex-1">
+                <h1 class="text-2xl sm:text-3xl md:text-5xl font-bold mb-4 overflow-hidden">Find our locations</h1>
+                <p class="text-sm sm:text-base md:text-lg lg:text-base max-w-xl text-left">Serving fresh fish daily
+                    at Boston Harbor Islands, Boylston St, Congress St, Kendall Square, Cambridge St, Haviland St.
+                </p>
+                <button class="rounded-full text-white px-6 py-2 mt-5 sm:text-sm md:text-base"
+                    style="background:#F96540;">Locations & Hours</button>
+            </div>
+            <div class="flex md:flex items-center justify-center gap-4 md:justify-start">
+                <img class="hidden md:block max-w-[200px] w-full h-auto" src="public/images/Image1.png" alt="">
+                <img class="w-[120px] h-[120px] sm:w-[140px] sm:h-[140px] md:w-[174px] md:h-[174px]"
+                    src="public/images/food3.png" alt="">
             </div>
         </div>
     </section>
 
-        <section class="w-full py-20 h-full bg-white relative overflow-hidden mx-auto md:flex items-center jsutify-center mt-40">
-            <div class="max-w-6xl mx-auto flex items-center justify-center flex-col-reverse lg:flex-row gap-10 relative z-10">
 
+    <section class=" max-w-[1440px] mx-auto px-4 py-10">
+        <div class="flex lg:flex-row items-start gap-8">
+            <div class="w-[720px]  sticky top-10">
+                <h1 class="text-[18px] sm:text-2xl md:text-3x1 font-bold overflow-hidden">Follow us</h1>
+                <div class="text-[12px] sm:text-2xl md:text-2xl overflow-hidden mt-4">
 
-                <div class="flex-1 text-center lg:text-left overflow-hidden">
-                    <h1
-                        class="text-3xl md:text-4xl lg:text-[48px] font-semibold mb-4 leading-tight max-w-xl overflow-hidden">
-                        Traditional Heritage, <br class="hidden md:block" />Modern Result
-                    </h1>
-                    <p class="text-base md:text-lg text-gray-700 max-w-xl mx-auto lg:mx-0">
-                        One of the reasons for poke's popularity is its versatility. While the traditional version
-                        remains a favorite,
-                        there are now numerous variations available, allowing people to customize their poke bowls
-                        according to their
-                        preferences.
-                    </p>
+                    <p class="mt-2">@pokebarboston</p>
+                    <p class="mt-2">To stay updated with the latest news, promotions, and offerings from the poke
+                        restaurant, make sure to follow us social media accounts. Don't miss out on any updates</p>
                 </div>
-
-
-                <div
-                    class="hidden md:block lg:block sm:flex-1 relative  justify-center items-center w-full max-w-[400px] mx-auto overflow-hidden">
-
-                    <img src="public/images/Oval 1.png"
-                        class="w-full max-w-[300px] sm:max-w-[340px] md:max-w-[380px] lg:max-w-[400px] z-0"
-                        alt="Oval background" />
-
-
-                    <img src="public/images/f2.png"
-                        class="absolute top-1/2 left-1 w-[200px] sm:w-[230px] md:w-[260px] transform -translate-x-1/2 -translate-y-1/2 z-10"
-                        alt="Main Bowl" />
-
-
-                    <img src="public/images/f3.png"
-                        class="absolute top-[-40px] left-[200px] w-[80px] sm:w-[90px] md:w-[100px] z-20"
-                        alt="Leaf Decor" />
-
-
-                    <img src="public/images/f4.png"
-                        class="absolute bottom-[-30px] right-[-30px] w-[70px] sm:w-[80px] md:w-[90px] z-20"
-                        alt="Leaf Decor 2" />
+                <div class="flex flex-wrap mt-6 gap-4 sm:mb-4 ">
+                    <button><img src="public/images/Button 1 (1).png" alt=""></button>
+                    <button><img src="public/images/Button 2 (1).png" alt=""></button>
+                    <button><img src="public/images/Button 3.png" alt=""></button>
+                    <button><img src="public/images/Button 4.png" alt=""></button>
                 </div>
             </div>
-        </section>
-
-
-
-        <section class="h-full mx-auto px-4 md:px-10 py-10 max-w-[1440px] md:flex items-center justify-center">
-            <div class="w-full flex flex-col md:flex-row bg-black text-white px-6 py-8 rounded-lg gap-6">
-                <div class="flex-1">
-                    <h1 class="text-2xl sm:text-3xl md:text-5xl font-bold mb-4 overflow-hidden">Find our locations</h1>
-                    <p class="text-sm sm:text-base md:text-lg lg:text-base max-w-xl text-left">Serving fresh fish daily
-                        at Boston Harbor Islands, Boylston St, Congress St, Kendall Square, Cambridge St, Haviland St.
-                    </p>
-                    <button class="rounded-full text-white px-6 py-2 mt-5 sm:text-sm md:text-base"
-                        style="background:#F96540;">Locations & Hours</button>
+            <div class="flex gap-5 h-[616px]">
+                <div class="flex flex-col gap-6">
+                    <img src="public/images/v1.png" alt="">
+                    <img src="public/images/v2.png" alt="">
+                    <img src="public/images/v3.png" alt="">
                 </div>
-                <div class="flex md:flex items-center justify-center gap-4 md:justify-start">
-                    <img class="hidden md:block max-w-[200px] w-full h-auto" src="public/images/Image1.png" alt="">
-                    <img class="w-[120px] h-[120px] sm:w-[140px] sm:h-[140px] md:w-[174px] md:h-[174px]"
-                        src="public/images/food3.png" alt="">
+                <div class="flex flex-col gap-6">
+                    <img src="public/images/v4.png" alt="">
+                    <img src="public/images/v5.png" alt="">
+                    <img src="public/images/v6.png" alt="">
+                </div>
+                <div class="flex flex-col gap-6">
+                    <img src="public/images/v7.png" alt="">
+                    <img src="public/images/v8.png" alt="">
+                    <img src="public/images/v9.png" alt="">
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
 
-        <section class=" max-w-[1440px] mx-auto px-4 py-10">
-            <div class="flex lg:flex-row items-start gap-8">
-                <div class="w-[720px]  sticky top-10">
-                    <h1 class="text-[18px] sm:text-2xl md:text-3x1 font-bold overflow-hidden">Follow us</h1>
-                    <div class="text-[12px] sm:text-2xl md:text-2xl overflow-hidden mt-4">
 
-                        <p class="mt-2">@pokebarboston</p>
-                        <p class="mt-2">To stay updated with the latest news, promotions, and offerings from the poke
-                            restaurant, make sure to follow us social media accounts. Don't miss out on any updates</p>
-                    </div>
-                    <div class="flex flex-wrap mt-6 gap-4 sm:mb-4 ">
-                        <button><img src="public/images/Button 1 (1).png" alt=""></button>
-                        <button><img src="public/images/Button 2 (1).png" alt=""></button>
-                        <button><img src="public/images/Button 3.png" alt=""></button>
-                        <button><img src="public/images/Button 4.png" alt=""></button>
-                    </div>
-                </div>
-                <div class="flex gap-5 h-[616px]">
-                    <div class="flex flex-col gap-6">
-                        <img src="public/images/v1.png" alt="">
-                        <img src="public/images/v2.png" alt="">
-                        <img src="public/images/v3.png" alt="">
-                    </div>
-                    <div class="flex flex-col gap-6">
-                        <img src="public/images/v4.png" alt="">
-                        <img src="public/images/v5.png" alt="">
-                        <img src="public/images/v6.png" alt="">
-                    </div>
-                    <div class="flex flex-col gap-6">
-                        <img src="public/images/v7.png" alt="">
-                        <img src="public/images/v8.png" alt="">
-                        <img src="public/images/v9.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </section>
-
-
-   
 
 
 
